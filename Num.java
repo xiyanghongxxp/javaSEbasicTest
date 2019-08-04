4f4b5a9e6d7fc4ab2f643f1542b46eb92c72967d
yjr <yjr962464@163.com>
2019-08-04 20:11:54 +0800

    test

---
+++ Num.java
@@ -0,0 +1,11 @@
+
+public class Num{
+    public static void main (String[] args){
+	long num1 = 1234567801L;
+	float num2 = 3.1415926f;
+	double num3 = 1234567890.0987654321d;
+	System.out.print(num1+"\n");
+	System.out.print(num2+"\n");
+	System.out.print(num3+"\n");
+    }
+}
