bc2d99dd9436560e9978e08cdacb49e4f769c2e4
yjr <yjr962464@163.com>
2019-08-04 20:11:03 +0800

    test

---
+++ Jframe.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Jframe{
+    public static void main (String[] args){
+  for(char a=1;a<50000;a++){
+   char x=a;
+   System.out.print(x);
+  }
+    }
+}
