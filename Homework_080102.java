fd32337d4f900e5b75718d3ee0b3809cd795915d
yjr <yjr962464@163.com>
2019-08-04 20:07:40 +0800

    test

---
+++ Homework_080102.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Homework_080102{
+	public static void main(String []args){
+		System.out.println(getValue());
+	}
+	public static double getValue(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("«Î ‰»ÎXµƒ÷µ£∫");
+		double a = scanner.nextDouble();
+		double y = Math.pow(a,2)-2*a+4;
+		return y;
+	}
+}
