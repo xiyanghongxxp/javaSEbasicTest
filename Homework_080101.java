9f9a4d157c744b958fdf096e48b28dbe5c6492ef
yjr <yjr962464@163.com>
2019-08-04 20:06:58 +0800

    test

---
+++ Homework_080101.java
@@ -0,0 +1,8 @@
+public class Homework_080101{
+	public static void main(String []args){
+		int a = 48;
+		System.out.println("48转换后为："+(char)a);
+		char b = '中';
+		System.out.println("中字转换后为："+(int)b);
+	}
+}
