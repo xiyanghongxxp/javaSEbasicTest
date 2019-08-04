17183b750c2b3cf5944e435459d46e52743a1c4e
yjr <yjr962464@163.com>
2019-08-04 20:09:18 +0800

    test

---
+++ Homework_080105.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Homework_080105{
+	public static void main(String []args){
+		Scanner scanner = new Scanner(System.in);
+		int score = scanner.nextInt();
+		
+		if(score > 100 || score < 0){
+			System.out.println("super man");
+		}else if(score >= 90 & score <= 100){
+			System.out.println("优秀");
+		}else if(score >= 80 & score < 90){
+			System.out.println("良");
+		}else{
+			System.out.println("不及格");
+		}
+	}
+}
