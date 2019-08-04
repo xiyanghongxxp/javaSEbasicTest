7c25301477bb2fc62c635314415302c8b8b8081b
yjr <yjr962464@163.com>
2019-08-04 20:02:36 +0800

    test

---
+++ ALBaBa.java
@@ -0,0 +1,9 @@
+
+public class ALiBaBa{
+	public static void main(String []args){
+		System.out.println("二进制数字为"+Integer.toBinaryString(19));
+		System.out.println("八进制数字为"+Integer.toOctalString(19));
+		System.out.println("十六进制数字为"+Integer.toHexString(19));
+	}
+}
+		
