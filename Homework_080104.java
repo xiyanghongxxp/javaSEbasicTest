a99da1aaaac301b5176a1ba906681c8072a586cf
yjr <yjr962464@163.com>
2019-08-04 20:08:57 +0800

    test

---
+++ Homework_080104.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Homework_080104{
+	public static void main(String []args){
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = 10;
+		double c = a%b==0 ? a%b : a/b+1;
+		System.out.println(c);
+	}
+}
